6bb2d07ad2bdc75594b9d4f4d67a9239659847e4
Eric P. Stavarache <ericptst@gmail.com>
2015-10-23 18:20:17 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+    response.json({message: "hello"});
+});
+
+app.post('/faceapi/getEmotion', upload.single('photo'), function(req, res, next) {
+    var f = req.file;
+    res.json(process(f));
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
